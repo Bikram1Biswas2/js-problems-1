dd639fa7972cef1b238cc5e6658843219a3c4282
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-09-07 01:54:07 +0530

    No-duplicates item done

--- no-duplicates.js
+++ no-duplicates.js
@@ -1,5 +1,7 @@
 const places = ['London', 'Paris', 'New York', 'London', 'Paris', 'America', 'Los Angels', 'Paris' ]
 
+const nums = [2,4,6,8,4,2,5,8]
+
 function noDuplicate(array){
        const unique = [];  
     for(const item of array){
@@ -12,3 +14,7 @@ function noDuplicate(array){
 
 const uniqueArray = noDuplicate(places);
 console.log(uniqueArray);
+
+const uniqueArray2 = noDuplicate(nums);
+console.log(uniqueArray2);
+
